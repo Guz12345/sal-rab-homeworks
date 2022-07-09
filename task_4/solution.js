59a6c7d8af7141f10aa74b478ae49c744406143f
Guzaliya <guzaliyafat@gmail.com>
2022-07-09 21:17:43 +0300

    task #4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -3,18 +3,23 @@
 function parseProducts(json) {
     // Аргументом функции является JSON
 
-    let data = {JSON.parse(json)}; // Преобразуйте строку json, переданную как аргумент функции,
+    let data = JSON.parse(json); // Преобразуйте строку json, переданную как аргумент функции,
     let products = data.products; // в объект с помощью функции JSON.parse(json)
     return products; // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
 }
 
 function renderProductsCards(json){ // Напишите функцию renderProductsCards(json)
-// Аргументом функции является JSON
-clearProducts(renderProductsCards); // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
-}
-function parseProducts(json); // Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
-let length = products.length; // Запишите в переменную length значение свойства products.length
-for (let i = 0; i < length; i += 1) { // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
-    addProduct(products[i]);// и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+    clearProducts();
+    let products = parseProducts(json);
+    let length = products.length;
+    for (let i = 0; i < length; i += 1) {
+        addProduct(products[i]);
+    }
+    clearProducts(renderProductsCards) // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
+
+// Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
+// Запишите в переменную length значение свойства products.length
+// Напишите цикл, в котором перебираете все products от 0 до (length - 1)
+// и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
 }
