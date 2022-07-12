bf46f3ac1111e50f1ce2e82a5f7e769cee608a5e
Guzaliya <guzaliyafat@gmail.com>
2022-07-12 19:51:59 +0300

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -39,13 +39,15 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i += 1) {
-        let goods = {title:'Пицца', count:2};
-        data.goods.push(goods[i]);
+             
+        data.goods.push({title:goods[i].title, count:goods[i].count});
+        
+       
         
     }
 
     data.client = name + ' ' + phone;
-    data.order.address = 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, '  + ' кв. ' + address.flat;
+    data.order.address = 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, '  + 'кв ' + address.flat;
     data.order.sum = sum;
     
 
